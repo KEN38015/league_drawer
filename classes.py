88fae6bc2c137bab4289d736f4df79f4c0d8e3f4
Dinh Vu Hoang Quan <hoangquandinhvu@gmail.com>
2026-08-09 13:44:30 +0700

    completed displays, matchmaking and scoreline updates didnt finish imports

--- classes.py
+++ classes.py
@@ -1,3 +1,8 @@
+from time import sleep
+import random
+
+
+
 class Table:
 	# format {stat : (abbr, width)}
 	#					width 0 if custom / dependent
@@ -16,26 +21,27 @@ class Table:
 		"goals conceded" : ("GA", 2),
 		"goal difference" : ("GD", 2),
 	}
-	
 	stats_showing = [
 		"matches played",
 		"wins",
 		"draws",
 		"losses",
 		"points",
+		
 	]
 
 	def __init__(self, 
 				title : str,) -> None:
 		self.title : str = title
-		self.teams : List[Team] = [Team("LIV"), Team("NOTTM FOREST")]
+		self.teams : List[Team] = []
+		self.matchups = []
 		self.started : bool = False
-		self.both_ways = input("Play home and away?\n")
-		while self.both_ways.lower() not in ("y", "n", "yes", "no"):
-			self.both_ways = input("Play home and away?\n")
-
+		self.sort_table()
 
 	def add_team(self, team : Team) -> None:
+		if len(self.teams) >= 99:
+			print("Limit Reached!")
+			return
 		self.teams.append(team)
 
 
@@ -45,27 +51,111 @@ class Table:
 		self.teams.remove(team)
 		return False
 
+
+	def add_matchup(self, to_add : Tuple(Team)) -> None:
+		self.matchups.append(to_add)
+
+	def remove_matchup(self, to_remove : Tuple(Team)) -> None:
+		self.matchups.remove(to_remove)
+
+	def import_matchups(self) -> list:
+		pass
+
+
+	def create_matchups(self, auto : bool) -> list:
+		pairings = []
+		remaining = self.teams[:]
+		if auto:
+			for home in self.teams[::-1]:
+				for away in remaining:
+					if home == away:
+						continue
+					pairings.append([home, away])
+				remaining.remove(home)
+			n = len(pairings)
+			return random.sample(pairings, n) + list(map(lambda pair: pair[::-1], random.sample(pairings, n)))
+		else:
+			while (choice := input("import?\n").lower()) not in ["y", "n", "yes", "no"]:
+				pass
+			sleep(.3)
+			if "y" in choice:
+				return import_matchups()
+
+			codes = {list(map(lambda x: x.code, self.teams))}
+			
+			n = len(self.teams)
+			
+			team_matches = (n-1)*(2)
+			home_available = [team_matches] * n
+			away_available = [team_matches] * n
+			total = n*team_matches
+			for i in range(total):
+				print(f"{i}/{total} matches completed")
+				sleep(.3)
+				print(f"teams available for home: {" ".join([codes[ind] for ind in range(n) if home_available[ind]])}")
+				while (home := input("home team? (3-digit code)\n")) not in codes:
+					if home_available[codes.index(home)]:
+						break
+					sleep(.3)
+				home_index = codes.index(home)
+				home_available[codes.index(home)] -= 1
+				sleep(.5)
+				
+
+				print(f"teams available for away: {" ".join([codes[ind] for ind in range(n) if away_available[ind]].remove(home))}")
+				while (away := input("home team? (3-digit code)\n")) not in codes:
+					if away_available[codes.index(away)]:
+						break
+					sleep(.3)
+				away_index = codes.index(away)
+				away_available[codes.index(away)] -= 1
+
+				pairings.append([self.teams[home_index], self.teams[away_index]])
+
+			return
+
+
+
+
+
+	def start_season(self) -> None:
+		self.started = True
+		while (choice := input("Manual or Automatic (does not account stadium) match creation?\n").lower()) not in {"manual", "automatic", "auto"}:
+			pass
+		sleep(.3)
+		matchups = self.create_matchups("auto" in choice)
+		print(*matchups, sep="\n")
+
 	def sort_table(self) -> None:
 		if not self.started:
 			self.teams.sort(key=lambda t: (-t.points, -t.goal_difference, -t.away_goals))
-		self.teams.sort(key=Team.name)
+		self.teams.sort(key=lambda t: t.abbr)
+
+
 	
+
+	
+	# home then away
 	def add_result(self, data1 : Tuple[Team, int], data2 : Tuple[Team, int]):
 		team1, res1 = data1
 		team2, res2 = data2
+
+		team1.add_scoreline(res1, res2, home=True)
+		team2.add_scoreline(res2, res1, home=False)
 			# /unfinished	!!!/
 
 
 
 	def __str__(self) -> str:
 
+		self.sort_table()
 		total_width : int = sum(
 						[
 						5, # rank column
-						len(max(self.teams, key=lambda x: x.name).name) + 2, # longest name + 2 trailings
+						len(max(self.teams, key=lambda x: len(x.abbr)).abbr) + 2, # longest name + 2 trailings
 						sum(
 							
-							list(map(lambda x: self.existing_stats[x][-1] * 2 + len(self.existing_stats[x][0]), self.stats_showing))
+							list(map(lambda x: self.existing_stats[x][1] * 2 + len(self.existing_stats[x][0]), self.stats_showing))
 							
 						),
 						len(self.stats_showing) # number of posts
@@ -75,23 +165,39 @@ class Table:
 		center = total_width // 2
 		lines : list = []
 
+		def border(char : str) -> str:
+			return str(" " + char * (total_width - 1))
+
+
 		# title
 		lines.append(" " * (center - len(self.title)//2) + self.title.upper())
 		
-		lines.append("_" * total_width)
+		lines.append(border("_"))
 
 
 		identifiers : list = ["| R |"]
-		longest_name = len(max(self.teams, key=lambda x: x.name).name) + 2
+		
+		longest_name = len(max(self.teams, key=lambda x: len(x.abbr)).abbr) + 2
 		identifiers.append(" " * (longest_name // 2 - 2) + "Name" + " " * (longest_name // 2 - 2) + "|")
 		for stat in self.stats_showing:
 			key, trailings = self.existing_stats[stat]
 			identifiers.append(" " * trailings + key + " " * trailings + "|")
 			
-		lines.append("".join(identifiers))
-		lines.append(" " + "=" * (total_width-2) + " ")
+		lines.append("".join(identifiers) + "")
+		lines.append(border("="))
+
+		for rank, team in enumerate(self.teams):
+			text = [f"| {rank+1}{" " if rank+1 < 10 else ""}", f" {" ".join(list(map(str.capitalize, team.abbr.split())))}" + " " * (longest_name - len(team.abbr) - 1)]
+			info = team.get_info()
+			for stat in self.stats_showing:
+				trailings = self.existing_stats[stat][1]
+				text.append(" " * trailings + str(info[stat]) + " " * (trailings + len(self.existing_stats[stat][0]) - len(str(info[stat]))))
 
 
+
+			lines.append("|".join(text) + "|")
+
+		lines.append(border("="))
 		return "\n".join(lines)
 
 
@@ -99,17 +205,15 @@ class Table:
 			 		PREMIER LEAGUE
  _____________________________________________________
 | R |     Name     |  MP  |  W  |  D  |  L  |   Pts   |
-
-| R |     Name     |  MP  |  W  |  D  |  L  |   Pts   |
  =====================================================
-| 1 | LIVERPOOL    |  38  | 25  |  9  |  4  |   84    |
-| 2 | ARSENAL      |  38  | 20  |  14 |  4  |   74    |
-| 3 | MAN CITY     |  38  | 21  |  8  |  9  |   71    |
-| 4 | CHELSEA      |  38  | 20  |  9  |  9  |   69    |
-| 5 | NEWCASTLE    |  38  | 20  |  6  |  12 |   66    |
-| 6 | ASTON VILLA  |  38  | 19  |  9  |  10 |   66    |
-| 7 | NOTTM FOREST |  38  | 19  |  8  |  11 |   65    |
-======================================================
+| 1 | Liverpool    |  38  |  25 |  9  |  4  |   84    |
+| 2 | Arsenal      |  38  |  20 |  14 |  4  |   74    |
+| 3 | Man City     |  38  |  21 |  8  |  9  |   71    |
+| 4 | Chelsea      |  38  |  20 |  9  |  9  |   69    |
+| 5 | Newcastle    |  38  |  20 |  6  |  12 |   66    |
+| 6 | Aston Villa  |  38  |  19 |  9  |  10 |   66    |
+| 7 | Nottm Forest |  38  |  19 |  8  |  11 |   65    |
+ =====================================================
 
 
 
@@ -126,29 +230,65 @@ class Team:
 	
 	def __init__(self,
 				name : str,
+				abbreviation : str,
+				code : str,
 				):
 		self.name : str = name
+		self.abbr : str = abbreviation.capitalize()
+		self.code : str = code
 		self.points : int = 0
 		self.wins : int = 0 ; self.draws : int = 0 ; self.losses : int = 0
 		self.matches_played : int = 0
 		self.home_goals : int = 0 ; self.away_goals : int = 0
 		self.goals_scored : int = 0 ; self.goals_conceded : int = 0 ; self.goal_difference : int = 0
 
-
-	def calculate() -> None:
-		pass
-
-
-	def get_info() -> List:
-
-		return [
-
-		]
+	# resets dependencies
+	def recalibrate(self) -> None:
+		self.points = 3 * self.wins + self.draws
+		self.matches_played = self.wins + self.draws + self.losses
+		self.goals_scored = self.home_goals + self.away_goals
+		self.goal_difference = self.goals_scored - self.goals_conceded
 
 
+	def add_scoreline(self, scored : int, conceded : int, *, home : bool) -> None:
+		state = "lose" if scored < conceded else ("win" if scored > conceded else "draw")
+		if home:
+			self.home_goals += scored
+		else:
+			self.away_goals += scored
+		
+		match state:
+			case "win":
+				self.wins += 1
+			case "draw":
+				self.draws += 1
+			case "lose":
+				self.losses += 1
+
+		self.recalibrate()
+
+	def get_info(self) -> dict:
+
+		return {
+		# "name" : ("Name", 0),
+		"points" : self.points,
+		"wins" : self.wins,
+		"draws" : self.draws,
+		"losses" : self.losses,
+		"matches played" : self.matches_played,
+		"home goals" : self.home_goals,
+		"away goals" : self.away_goals,
+		"goals scored" : self.goals_scored,
+		"goals conceded" : self.goals_conceded,
+		"goal difference" :self.goal_difference,
+		}
+
+	def __eq__(self, other) -> bool:
+		return self.name == other.name
+
+
+	def __str__(self) -> str:
+		return self.name
 
 
 
-
-i = Table("PL")
-print(i)
---
+++ main.py
@@ -0,0 +1,62 @@
+from classes import *
+import random
+
+
+
+
+
+
+
+
+
+
+def main() -> None:
+	premier_league = Table("Premier League")
+	clubs = {
+    "Arsenal" : 				Team("Arsenal", "Arsenal", "ARS"),
+    "Aston Villa" :				Team("Aston Villa", "Aston Villa", "AVL"),
+    "Bournemouth": 				Team("Bournemouth", "Bournemouth", "BOU"),
+    "Brentford" : 				Team("Brentford", "Brentford", "BRE"),
+    "Brighton & Hove Albion" : 	Team("Brighton & Hove Albion", "Brighton", "BHA"),
+    "Chelsea" : 				Team("Chelsea", "Chelsea", "CHE"),
+    "Crystal Palace" : 			Team("Crystal Palace", "Crystal Palace", "CRY"),
+    "Everton" : 				Team("Everton", "Everton", "EVE"),
+    "Fulham" : 					Team("Fulham", "Fulham", "FUL"),
+    "Leeds United" : 			Team("Leeds United", "Leeds", "LEE"),
+    "Liverpool" : 				Team("Liverpool", "Liverpool", "LIV"),
+    "Manchester City" : 		Team("Manchester City", "Man City", "MCI"),
+    "Manchester United" : 		Team("Manchester United", "Man Utd", "MUN"),
+    "Newcastle United" : 		Team("Newcastle United", "Newcastle", "NEW"),
+    "Nottingham Forest" : 		Team("Nottingham Forest", "Nott'm Forest", "NFO"),
+    "Sunderland" : 				Team("Sunderland", "Sunderland", "SUN"),
+    "Tottenham Hotspur" : 		Team("Tottenham Hotspur", "Spurs", "TOT"),
+    "Coventry City" : 			Team("Coventry City", "Coventry", "COV"),
+    "Ipswich Town" : 			Team("Ipswich Town", "Ipswich", "IPS"),
+    "Hull City" : 				Team("Hull City", "Hull", "HUL"),
+}
+	for team, data in clubs.items():
+		premier_league.add_team(data)
+	print(premier_league)
+	premier_league.start_season()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
